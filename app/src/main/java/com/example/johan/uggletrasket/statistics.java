ba732a0f7bf240135abc6447969eb784d1b1dbc5
David Kron <krdavid@student.chalmers.se>
2015-05-20 08:21:24 +0200

    Back button on result view

--- app/src/main/java/com/example/johan/uggletrasket/statistics.java
+++ app/src/main/java/com/example/johan/uggletrasket/statistics.java
@@ -1,9 +1,12 @@
 package com.example.johan.uggletrasket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -25,6 +28,7 @@ public class Statistics extends Activity {
     /** Called when the activity is first created. */
 
     TextView resultView;
+    private Button returnButton;
     ScrollView mScrollView;
     String quizID = "";
     QuestionList ql;
@@ -35,7 +39,8 @@ public class Statistics extends Activity {
         setContentView(R.layout.activity_statistics);
         StrictMode.enableDefaults(); //STRICT MODE ENABLED
 
-        resultView= (TextView) findViewById(R.id.result);
+        resultView = (TextView) findViewById(R.id.result);
+        returnButton = (Button) findViewById(R.id.returnStatsButton);
 
         //Get quizID
         Bundle quizInfo = getIntent().getExtras();
@@ -45,6 +50,15 @@ public class Statistics extends Activity {
         
         ql =  LoadQuestions.getData(getResources().getString(R.string.getQuizQuestions), quizID);
         printResult();
+
+        View.OnClickListener listAdd = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Statistics.this, MainActivity.class));
+            }
+        };
+
+        returnButton.setOnClickListener(listAdd);
         
     }
 
